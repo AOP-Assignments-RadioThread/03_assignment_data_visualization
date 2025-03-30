c878fcc86e39e550eeec4d8b140d3aab4495c778
vld31 <vlad.tomulescu317@gmail.com>
2025-03-30 15:19:59 +0200

    ModelImplemented

---
+++ InSight/InSight/Models/GlobalFoodWastageModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InSight.Models
+{
+    class GlobalFoodWastageModel
+    {
+        public string Country { get; set; }
+        public int Year { get; set; }
+        public string FoodCategory { get; set; }
+        public double TotalWaste { get; set; }
+        public double EconomicLoss { get; set; }
+        public double AvgWaste { get; set; }
+        public double Population { get; set; }
+        public double HouseholdWaste { get; set; }
+    }
+}
---
+++ InSight/InSight/Models/ModelMapper.cs
@@ -0,0 +1,18 @@
+﻿using CsvHelper.Configuration;
+
+namespace InSight.Models
+{
+    class ModelMapper : ClassMap<GlobalFoodWastageModel>
+    {
+        public ModelMapper() 
+        {
+            Map(m => m.Country).Name("Country");
+            Map(m => m.Year).Name("Year");
+            Map(m => m.FoodCategory).Name("Food Category");
+            Map(m => m.TotalWaste).Name("Total Waste (Tons)");
+            Map(m => m.AvgWaste).Name("Avg Waste per Capita (Kg)");
+            Map(m => m.Population).Name("Population (Million)");
+            Map(m => m.HouseholdWaste).Name("Household Waste (%)");
+        }    
+    }
+}
