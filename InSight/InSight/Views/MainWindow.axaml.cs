82dc3613c2428e6329e40ec162990354b69994e4
Victor <victorptrc@users.noreply.github.com>
2025-03-30 14:31:27 +0200

    initial layout

--- InSight/InSight/Views/MainWindow.axaml.cs
+++ InSight/InSight/Views/MainWindow.axaml.cs
@@ -7,5 +7,9 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        Button ghena = new Button();
+        ghena.Content = "Ghena";
+        
+        ChartContainer.Children.Add(ghena);
     }
 }
